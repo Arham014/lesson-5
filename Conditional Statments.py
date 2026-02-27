715c303e721daa63cb2a3c75e62280c6dc77e52a
Arham014 <mjalalhussain@yahoo.com>
2026-02-27 06:26:42 -0800

    Add files via upload

---
+++ Conditional Statments.py
@@ -0,0 +1,6 @@
+temp=float(input("Please enter ambient temperature : "))
+x=35
+if(temp>35):
+    print("Temperature greater than 35 ,wear soft clothes")
+else:
+    print("Temperature smaller then 35 ,wear heavy clothes")
